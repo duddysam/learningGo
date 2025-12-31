715061d8e2272f3205efe96d5ea7f1f294668675
duddysam <duddysam91@gmail.com>
2025-12-31 14:07:43 -0500

    finish exercises for ch2

--- ch2/exercises.go
+++ ch2/exercises.go
@@ -5,5 +5,15 @@ import "fmt"
 func main() {
 	var i int = 20
 
-	fmt.Println(i)
+	var f = float64(i)
+
+	fmt.Printf("printing var i int = %d\n", i)
+	fmt.Printf("printing var f = %f\n", f)
+
+	const value = 10
+
+	var y int = value
+	var z float64 = value
+
+	fmt.Printf("checking that var y = %d and var z = %f are valid", y, z)
 }
