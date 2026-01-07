c79375e7f2eeaedce061560ca2ad18e1077f81b5
duddysam <duddysam91@gmail.com>
2026-01-07 10:07:57 -0500

    complete problem 2 in ch4

--- ch4/exercises.go
+++ ch4/exercises.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"unicode"
 )
 
 func formatCardNum(cardNum string) (string, error) {
@@ -20,9 +21,50 @@ func formatCardNum(cardNum string) (string, error) {
 
 }
 
+func nameValidator(name string) (bool, error) {
+
+	nameRunes := []rune(name)
+
+	if len(nameRunes) < 2 || len(nameRunes) > 50 {
+		return false, errors.New("Invalid name length")
+	}
+
+	for i, r := range nameRunes {
+
+		if !unicode.IsLetter(r) && (r != ' ' && r != '\'' && r != '-') {
+			return false, errors.New("Invalid Character:")
+		}
+		if (i == 0 || i == len(nameRunes)-1) && r == ' ' {
+			return false, errors.New("Cannot start or end with a space")
+		}
+	}
+
+	return true, nil
+}
+
 func main() {
 
+	// TEST PROBLEM 1
 	formattedNum, err := formatCardNum("1234567890123456")
 
 	fmt.Println(formattedNum, err)
+
+	// TEST PROBLEM 2
+	valid, err := nameValidator("Sam Duddy")
+	fmt.Println("true ==", valid, err)
+
+	valid, err = nameValidator(" Sam Duddy")
+	fmt.Println("false ==", valid, err)
+
+	valid, err = nameValidator("Sam Duddy ")
+	fmt.Println("false ==", valid, err)
+
+	valid, err = nameValidator("Sam Duddy's")
+	fmt.Println("true ==", valid, err)
+
+	valid, err = nameValidator("Sam Duddy-s")
+	fmt.Println("true ==", valid, err)
+
+	valid, err = nameValidator("Sam Duddy$s")
+	fmt.Println("false ==", valid, err)
 }
