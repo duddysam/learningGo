1daa73ae6aaab76a922783ec3f822b0ba7897c09
duddysam <duddysam91@gmail.com>
2026-01-12 10:20:28 -0500

    complete problem 1 and 2 ch6

--- ch6/ch6Exercises.go
+++ ch6/ch6Exercises.go
@@ -2,7 +2,28 @@ package main
 
 import "fmt"
 
+func convertCentToDollar(centVal int) {
+	dollarVal := float64(centVal) / 100.00
+	fmt.Printf("Transaction amount: $%.2f\n", dollarVal)
+}
+
 func main() {
 
-	fmt.Println("Hello World!")
+	// PROBLEM 1
+	var txnID string
+	var amt float64
+	var currCode string
+	var mName string
+	var isApproved bool
+
+	txnID = "M101"
+	amt = 100.01
+	currCode = "USD"
+	mName = "payPal"
+	isApproved = true
+
+	fmt.Printf("txn: %s of $%.2f, %s through %s, has status: %t\n", txnID, amt, currCode, mName, isApproved)
+
+	// PROBLEM 2
+	convertCentToDollar(1050)
 }
